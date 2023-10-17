8c81a187be58dff001295682474a57623f474109
dungnh <nguyenhoangdung300993@gmail.com>
2023-10-17 19:47:50 +0700

    update migration

--- migrations/20231012001122_init_db.up.sql
+++ migrations/20231012001122_init_db.up.sql
@@ -2,7 +2,7 @@ START TRANSACTION;
 
 CREATE TABLE users
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
     name       VARCHAR(64)                         NOT NULL,
     status     VARCHAR(16)                         NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -11,11 +11,11 @@ CREATE TABLE users
 
 CREATE TABLE user_accounts
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
-    user_id    INT                                 NOT NULL,
+    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id    BIGINT                              NOT NULL,
     name       VARCHAR(64)                         NOT NULL,
     status     VARCHAR(16)                         NOT NULL,
-    balance DOUBLE NOT NULL,
+    balance    DOUBLE PRECISION                    NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
 );
